4ef28ade274bc202dd6de508704671aa451bff7a
hnalabanda <hemanalabanda@gmail.com>
2020-02-07 16:41:01 +0000

    Added new FileImporter class

---
+++ SupportBank/FileImporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+namespace SupportBank
+{
+    public class FileImporter
+    {
+        private string inputFile { get; set; }
+
+        public string GetImportFile()
+        {
+             inputFile = Console.ReadLine();
+             var fileInfo=new FileInfo(inputFile);
+             return fileInfo.Extension;
+
+        }
+        
+        public 
+       
+    }
+}
