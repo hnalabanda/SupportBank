57ed39809744145fb13748dabe8533e22957a610
hnalabanda <hemanalabanda@gmail.com>
2020-02-07 16:41:37 +0000

    Modified Program class

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -3,27 +3,40 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.VisualBasic;
-
+using NLog.Config;
+using NLog.Targets;
+using NLog.Attributes;
+using NLog;
 namespace SupportBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-         
-            
-            var reader = new TransactionReader();
-            List<Transaction> transactions = reader.GetTransactionsFromFile();
-            
-            var bank=new Bank("Softwire Support Bank");
-            bank.ProcessTransactions(transactions);
-            // int lineCounter = 1;
-      
-            
-            //Print Report
-            
-            Print print=new Print();
-            print.PrintReport(readCommand,bank);
+            var config = new LoggingConfiguration();
+            var target = new FileTarget
+                {FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}"};
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+
+            try
+            {
+                var reader = new TransactionReader();
+                List<Transaction> transactions = reader.GetTransactionsFromFile();
+                
+                var bank = new Bank("Softwire Support Bank");
+                bank.ProcessTransactions(transactions);
+                
+                //Print Report
+                Print print = new Print();
+                print.PrintReport(bank);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Occured, please check logs");
+            }
+
         }
     }
 }
