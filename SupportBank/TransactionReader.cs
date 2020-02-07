e0f0365fff1d641611402ba19088bbd00fd95b58
hnalabanda <hemanalabanda@gmail.com>
2020-02-07 16:41:57 +0000

    Added a Transaction reader class

--- SupportBank/TransactionReader.cs
+++ SupportBank/TransactionReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using NLog;
 namespace SupportBank
 {
     public class TransactionReader
@@ -13,22 +14,40 @@ namespace SupportBank
             this.transactions=new List<Transaction>();
         }
 
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        
         public List<Transaction> GetTransactionsFromFile()
         {
-            var lines = File.ReadAllLines(@"C:\Training\file.csv").Skip(1);
-            foreach (string line in lines)
+            Logger.Info("Reading transactions");
+            
+          //  var inputFile=new FileImporter();
+          // inputFile.GetImportFile();
+            
+            var inputFile = Console.ReadLine();
+            var lines = File.ReadAllLines(inputFile).Skip(1);
+            
+            var lineCounter = 0;
+            try
             {
-                var values = line.Split(",");
-                var newTransaction = new Transaction
+                foreach (string line in lines)
                 {
-                    transactionDate = Convert.ToDateTime(values[0]),
-                    fromIndividual = values[1],
-                    toIndividual = values[2],
-                    narrative = values[3],
-                    amount = Convert.ToDouble(values[4])
-                };
-                
-                transactions.Add(newTransaction);
+                    var values = line.Split(",");
+                    lineCounter += 1;
+                        var newTransaction = new Transaction
+                        {
+                            transactionDate = Convert.ToDateTime(values[0]),
+                            fromIndividual = values[1],
+                            toIndividual = values[2],
+                            narrative = values[3],
+                            amount = Convert.ToDouble(values[4])
+                        };
+                        transactions.Add(newTransaction);
+                }
+            }
+            catch(System.FormatException exception)
+            {
+                Logger.Error($"An error occured on line {lineCounter-1}: {exception.Message}");
+                throw exception;
             }
 
             return transactions;
